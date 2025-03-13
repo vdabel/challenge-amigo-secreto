6dcd11892721a7da4bf7b41ec12e7331492e8c61
Abel Enrique Valdivia Daza <132954955+vdabel@users.noreply.github.com>
2025-03-13 00:16:06 -0500

    Implementacion de funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const txtAmigo = document.getElementById("amigo");
+
+function agregarAmigo() {
+    let amigo = txtAmigo.value.trim();
+
+    if (amigo === "") {
+        alert("Por favor, inserte su nombre");
+    } else {
+        amigos.push(amigo);
+        txtAmigo.value = "";
+    }
+    console.log(amigos)
+    return;
+}
